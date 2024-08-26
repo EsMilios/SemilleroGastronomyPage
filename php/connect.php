c2da683c3893b104162ffef17785547bee441773
Emiliano <emilianoalejandrocalderon@gmail.com>
2024-08-26 12:51:10 -0500

    firts commit

--- php/connect.php
+++ php/connect.php
@@ -1,3 +1,17 @@
 <?php
-    $varConexion=mysqli_connect('localhost','root', '', 'gastronomy');
+$host = "mysql.railway.internal";
+$dbname = "railway";
+$username = "root";
+$password = "gDDLTJUMZTJEUygxRkrKiQdPBskacirc";
+$port = 3306;
+
+// Crear la conexión
+$conn = new mysqli($host, $username, $password, $dbname, $port);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+echo "Conexión exitosa!";
 ?>
